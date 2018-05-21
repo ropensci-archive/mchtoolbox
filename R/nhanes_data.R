34359d24e1935907fda9f29c328de25032565475
W. Kyle Hamilton <kyle.hamilton@gmail.com>
2018-05-21 15:36:11 -0700

    added nhanes data and docs

---
+++ R/nhanes_data.R
@@ -0,0 +1,14 @@
+#' National Health and Nutrition Examination Survey (NHANES) example dataset
+#'
+#' NHANES data.
+#'
+#' @format A dataframe.
+#' \describe{
+#'   \item{cid}{Patient identifier.}
+#'   \item{sex}{1 = male, 2 = female}
+#'   \item{agemons}{Age in months.}
+#'   \item{height}{Height in cm.}
+#'   \item{weight}{Weight in kg.}
+#'   \item{headcir}{Head circumference in cm.}
+#' }
+"nhanes_data"
