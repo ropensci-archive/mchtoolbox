1e640fe76991148b921d0b775bd4eceb8ead69ee
W. Kyle Hamilton <kyle.hamilton@gmail.com>
2018-05-25 00:15:43 -0700

    ggplot2

--- R/launch_mchtoolbox.R
+++ R/launch_mchtoolbox.R
@@ -4,7 +4,7 @@
 #' @description lauch_mchtoolbox loads an interactive user interface built using R shiny.
 #' @details The purpose of building the interactive user interface is to provide an easy method for people to use instead of the command line.
 #' @keywords mchtoolbox
-#' @import shiny shinythemes
+#' @import shiny shinythemes ggplot2
 #' @examples
 #' \dontrun{
 #' library(shiny)
