059bb9480cb9c86cb913d1d8497a0471104cf706
W. Kyle Hamilton <kyle.hamilton@gmail.com>
2018-06-01 21:06:25 -0700

    added tests for cdc_growth

--- tests/testthat/test-cdc_growth.R
+++ tests/testthat/test-cdc_growth.R
@@ -1,5 +1,24 @@
 context("test-cdc_growth.R")
 
 test_that("Returns data.frame", {
+
   expect_is(create_cdc_growth(nhanes_data), "data.frame")
+  expect_is(create_cdc_growth(nhanes_data), "tbl_df")
+  expect_length(create_cdc_growth(nhanes_data), 79)
+
+})
+
+test_that("Returns errors", {
+
+  expect_error(create_cdc_growth(1), "`data` must include columns `agemos`, `weight`, and `height`", fixed=TRUE)
+
+})
+
+test_that("Function returns correct results", {
+
+  test_data <- create_cdc_growth(nhanes_data)
+  data_test_1 <- as.numeric(test_data[2,37])
+  expect_true(test_data[2,2] == 1)
+  expect_equal(data_test_1, 48.72065, tolerance = .002)
+
 })
